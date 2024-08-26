f2ad0f91bea3759c4e72f8107fefaa3434fab84f
GIJoeCodes <31097875+gijoecodes@users.noreply.github.com>
2024-08-26 10:48:21 -0400

    initial navigation setup

--- Step Tracker/ContentView.swift
+++ Step Tracker/ContentView.swift
@@ -2,33 +2,79 @@
 //  ContentView.swift
 //  Step Tracker
 //
-//  Created by Jose on 8/23/24.
+//  Created by Jose Carrillo on 8/23/24.
+//  Built using Swift 5.0
+//
+//
+//  Redistribution or use in source and binary forms, with or without
+//  modification, are strictly prohibited without explicit written permission
+//  from the copyright holder.
+//
+//  You may not use, copy, modify, merge, publish, distribute, sublicense,
+//  create a derivative work, and/or sell copies of the software in any work
+//  that is designed, intended, or marketed for pedagogical or instructional
+//  purposes related to programming, coding, application development, or
+//  information technology. Permission for such use, copying, modification,
+//  merger, publication, distribution, sublicensing, creation of derivative works,
+//  or sale is expressly withheld.
+//
+//  Copyright © 2024 Jose. All rights reserved.
 //
 
 import SwiftUI
 
+enum HealthMetricContext: CaseIterable, Identifiable {
+    
+    case steps, weight
+    
+    var id: Self { self }
+    
+    var title: String {
+        switch self {
+        case .steps:
+            return "Steps"
+        case .weight:
+            return "Weight"
+        }
+    }
+    
+}
+
 struct ContentView: View {
+    
+    @State private var selectedStat: HealthMetricContext = .steps
+    var isSteps: Bool { selectedStat == .steps }
+    
     var body: some View {
         NavigationStack {
             ScrollView {
                 VStack(spacing: 20) {
-                    VStack {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Label("Steps", systemImage: "figure.walk")
-                                    .font(.title3.bold())
-                                    .foregroundStyle(.pink)
-                                
-                                Text("Avg: 10K Steps")
-                                    .font(.caption)
-                                    .foregroundStyle(.secondary)
-                            }
-                            
-                            Spacer()
-                            
-                            Image(systemName: "chevron.right")
-                                .foregroundStyle(.secondary)
+                    
+                    Picker("Selected Stat", selection: $selectedStat) {
+                        ForEach(HealthMetricContext.allCases) { metric in
+                            Text(metric.title)
                         }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    VStack {
+                        NavigationLink(value: selectedStat) {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Label("Steps", systemImage: "figure.walk")
+                                        .font(.title3.bold())
+                                        .foregroundStyle(.pink)
+                                    
+                                    Text("Avg: 10K Steps")
+                                        .font(.caption)
+                                }
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.right")
+                            }
+                        }
+                        .foregroundStyle(.secondary)
                         .padding(.bottom, 12)
                         
                         RoundedRectangle(cornerRadius: 12)
@@ -61,7 +107,11 @@ struct ContentView: View {
             }
             .padding()
             .navigationTitle("Dashboard")
+            .navigationDestination(for: HealthMetricContext.self) { metric in
+                Text(metric.title)
+            }
         }
+        .tint(isSteps ? .pink : .indigo)
     }
 }
 
