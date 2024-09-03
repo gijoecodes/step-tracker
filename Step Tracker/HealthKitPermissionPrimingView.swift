b8b3e338d66370c68e2d439ed9ddf4249fe9ae14
GIJoeCodes <31097875+gijoecodes@users.noreply.github.com>
2024-09-02 22:05:40 -0400

    permission priming ui

---
+++ Step Tracker/HealthKitPermissionPrimingView.swift
@@ -0,0 +1,64 @@
+//
+//  HealthKitPermissionPrimingView.swift
+//  Step Tracker
+//
+//  Created by Jose Carrillo on 9/2/24.
+//  Built using Swift 5.0
+//
+//
+//  Redistribution or use in source and binary forms, with or without
+//  modification, are strictly prohibited without explicit written permission
+//  from the copyright holder.
+//
+//  You may not use, copy, modify, merge, publish, distribute, sublicense,
+//  create a derivative work, and/or sell copies of the software in any work
+//  that is designed, intended, or marketed for pedagogical or instructional
+//  purposes related to programming, coding, application development, or
+//  information technology. Permission for such use, copying, modification,
+//  merger, publication, distribution, sublicensing, creation of derivative works,
+//  or sale is expressly withheld.
+//
+//  Copyright © 2024 Jose. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct HealthKitPermissionPrimingView: View {
+    
+    var description = """
+    This app displays your step and weight data in interactive charts.
+    
+    You can also add new step or weight data to Apple Health from this app. Your data is private and secured.
+    """
+    
+    var body: some View {
+        VStack(spacing: 130) {
+            VStack(alignment: .leading, spacing: 10) {
+                Image(.iconAppleHealth)
+                    .resizable()
+                    .frame(width: 90, height: 90)
+                    .shadow(color: .gray.opacity(0.3), radius: 16)
+                    .padding(.bottom, 12)
+                
+                Text("Apple Health Integration")
+                    .font(.title2).bold()
+                
+                Text(description)
+                    .foregroundStyle(.secondary)
+            }
+            
+            Button("Connect Apple Health") {
+                // do code later
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.pink)
+        }
+        .padding(30)
+    }
+    
+}
+
+#Preview {
+    HealthKitPermissionPrimingView()
+}
